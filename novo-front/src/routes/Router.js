94b9fac4b0869c110c2748947166602d701a3176
Re-Santos <renata.prog@gmail.com>
2024-02-08 22:53:25 -0300

    Login Style

--- novo-front/src/routes/Router.js
+++ novo-front/src/routes/Router.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import LoginPage from "../pages/LoginPage/LoginPage"
+import SignUpPage from "../pages/SignUpPage/SignUpPage"
 
 const Router = () => {
     return(
         <BrowserRouter>
             <Routes>
                 <Route index element={<LoginPage/>} />
+                <Route path='signup' element={<SignUpPage/>} />
                 
             </Routes>
         </BrowserRouter>
